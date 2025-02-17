09d437cb839f6b5638aa40ed26d443c2518a9f45
PhamHung02 <hungpsph49800@gmail.com>
2025-02-17 20:08:57 +0700

    du_an_1

---
+++ du_an_1/du_an_1/admin/index.php
@@ -0,0 +1,54 @@
+<?php 
+session_start();
+
+// Require file Common
+require_once '../commons/env.php'; // Khai báo biến môi trường
+require_once '../commons/function.php'; // Hàm hỗ trợ
+
+// Require toàn bộ file Controllers
+require_once './controllers/DashboardController.php';
+require_once './controllers/AdminThuongHieuController.php';
+require_once './controllers/AdminSanPhamController.php';
+
+
+
+
+
+// Require toàn bộ file Models
+require_once './models/AdminThuongHieu.php';
+require_once './models/AdminSanPham.php';
+
+
+
+
+// Route
+$act = $_GET['act'] ?? '/';
+
+
+// Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
+// Các Thầy.Cô có thể dùng Switch-Case
+    match ($act) {
+        // Trang chủ
+        '/' => (new DashboardController)->dashboard(),
+        
+        'thuong_hieu'=>(new AdminThuongHieuController())->danhSachThuongHieu(),
+        'form_them_thuong_hieu'=>(new AdminThuongHieuController())->formAddThuongHieu(),
+        'them_thuong_hieu'=>(new AdminThuongHieuController())->postAddThuongHieu(),
+        'form_sua_thuong_hieu'=>(new AdminThuongHieuController())->formEditThuongHieu(),
+        'sua_thuong_hieu'=>(new AdminThuongHieuController())->postEditThuongHieu(),
+        'xoa_thuong_hieu'=>(new AdminThuongHieuController())->deleteThuongHieu(),
+    
+        //san pham 
+        'san_pham'=>(new AdminSanPhamController())->danhSachSanPham(),
+        'form_them_san_pham'=>(new AdminSanPhamController())->formAddSanPham(),
+        'them_san_pham'=>(new AdminSanPhamController())->postAddSanPham(),
+        'form_sua_san_pham'=>(new AdminSanPhamController())->formEditSanPham(),
+        'sua_san_pham'=>(new AdminSanPhamController())->postEditSanPham(),
+        'xoa_san_pham'=>(new AdminSanPhamController())->deleteSanPham(),
+      
+
+        
+       
+    };
+
+
---
+++ du_an_1/du_an_1/commons/function.php
@@ -0,0 +1,101 @@
+<?php
+function debug($data)
+{
+    echo "<pre>";
+
+    print_r($data);
+
+    die;
+}
+
+// Kết nối CSDL qua PDO
+function connectDB() {
+    // Kết nối CSDL
+    $host = DB_HOST;
+    $port = DB_PORT;
+    $dbname = DB_NAME;
+
+    try {
+        $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", DB_USERNAME, DB_PASSWORD);
+
+        // cài đặt chế độ báo lỗi là xử lý ngoại lệ
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // cài đặt chế độ trả dữ liệu
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    
+        return $conn;
+    } catch (PDOException $e) {
+        echo ("Connection failed: " . $e->getMessage());
+    }
+}
+//thêm file
+function uploadFile($file, $folderUpload) {
+    $pathStorage=$folderUpload . time() . $file['name'];
+    $form=$file['tmp_name'];
+    $to = PATH_ROOT . $pathStorage;
+    if(move_uploaded_file($form, $to)) {
+        return $pathStorage;
+    }
+    return null;
+    
+}
+//sửa file
+//xóa file
+function deleteFile($file) {
+    $pathDelete = PATH_ROOT . $file;
+    if(file_exists($pathDelete)) {
+        unlink($pathDelete);
+    }  
+}
+// xóa session sau load trang
+// function deleteSessionError() {
+//     if(isset($_SESSION['flash'])) {
+//         unset($_SESSION['flash']);
+//         session_unset();
+//         // session_destroy();
+//     } 
+//     }
+function deleteSessionError() {
+    // Ensure session is started
+    // if (session_status() == PHP_SESSION_NONE) {
+    //     session_start();
+    // }
+
+    // Check if the flash message is set and then unset it
+    if (isset($_SESSION['flash'])) {
+        unset($_SESSION['flash']);
+        session_unset();
+        session_destroy();
+    }
+
+    // Unset all session variables
+   
+    
+    // Destroy the session
+    // session_destroy();
+}
+
+// Upload - updeatealbum ảnh
+function uploadFileAlbum($file, $folderUpload, $key) {
+    $pathStorage=$folderUpload . time() . $file['name'][$key];
+    $form=$file['tmp_name'][$key];
+    $to = PATH_ROOT . $pathStorage;
+    if(move_uploaded_file($form, $to)) {
+        return $pathStorage;
+    }
+    
+}
+// fomat date
+function formatDate($date) {
+    return date('d/m/Y', strtotime($date));
+}
+function checkLoginAdmin(){
+    if(!isset($_SESSION['admin'])) {
+        //không có session thi chuyển huỳ trang login
+        header('location: ' . BASE_URL_ADMIN . '?act=login_admin');
+        exit();
+    }
+}
+//debug
+
